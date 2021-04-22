4caa8542731f353931ac728133ad28dedb0a4911
MilicTG <milic.tg@gmail.com>
2021-04-22 12:07:19 +0200

    App finished

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:productivity_timer/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Container(
-        child: Text('Settings'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        body: Settings());
   }
 }
 
@@ -20,8 +19,31 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  TextEditingController txtWork;
+  TextEditingController txtShort;
+  TextEditingController txtLong;
+  SharedPreferences prefs;
+
+  static const String WORKTIME = "workTime";
+  static const String SHORTBREAK = "shortBreak";
+  static const String LONGBREAK = "longBreak";
+
+  int workTime;
+  int shortTime;
+  int longBreak;
+
+  @override
+  void initState() {
+    TextEditingController txtWork = TextEditingController();
+    TextEditingController txtShort = TextEditingController();
+    TextEditingController txtLong = TextEditingController();
+    readSettings();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    //textStyle
     TextStyle textStyle = TextStyle(fontSize: 24);
 
     return Container(
@@ -31,9 +53,114 @@ class _SettingsState extends State<Settings> {
         childAspectRatio: 3,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
-        children: <Widget>[],
         padding: const EdgeInsets.all(20.00),
+        children: <Widget>[
+          Text(
+            "Work",
+            style: textStyle,
+          ),
+          Text(''),
+          Text(''),
+          SettingButton(Color(0xff455A64), "-", -1, WORKTIME, updateSettings),
+          TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            controller: txtWork,
+          ),
+          SettingButton(Color(0xff009688), "+", 1, WORKTIME, updateSettings),
+          Text(
+            'Short',
+            style: textStyle,
+          ),
+          Text(''),
+          Text(''),
+          SettingButton(Color(0xff455A64), '-', -1, SHORTBREAK, updateSettings),
+          TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            controller: txtShort,
+          ),
+          SettingButton(Color(0xff009688), "+", 1, SHORTBREAK, updateSettings),
+          Text(
+            'Long',
+            style: textStyle,
+          ),
+          Text(''),
+          Text(''),
+          SettingButton(Color(0xff455A64), "-", -1, LONGBREAK, updateSettings),
+          TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            controller: txtLong,
+          ),
+          SettingButton(Color(0xff009688), "+", 1, LONGBREAK, updateSettings),
+        ],
       ),
     );
+  }
+
+  readSettings() async {
+    prefs = await SharedPreferences.getInstance();
+    int workTime = prefs.getInt(WORKTIME);
+    if (workTime == null) {
+      await prefs.setInt(WORKTIME, int.parse('30'));
+    }
+    int shortBreak = prefs.getInt(SHORTBREAK);
+    if (shortBreak == null) {
+      await prefs.setInt(SHORTBREAK, int.parse('5'));
+    }
+    int longBreak = prefs.getInt(LONGBREAK);
+    if (longBreak == null) {
+      await prefs.setInt(LONGBREAK, int.parse('20'));
+    }
+    setState(() {
+      txtWork.text = workTime.toString();
+      txtShort.text = shortBreak.toString();
+      txtLong.text = longBreak.toString();
+    });
+  }
+
+  void updateSettings(String key, int value) {
+    switch (key) {
+      case WORKTIME:
+        {
+          int workTime = prefs.getInt(WORKTIME);
+          workTime += value;
+          if (workTime >= 1 && workTime <= 180) {
+            prefs.setInt(WORKTIME, workTime);
+            setState(() {
+              txtWork.text = workTime.toString();
+            });
+          }
+        }
+        break;
+      case SHORTBREAK:
+        {
+          int short = prefs.getInt(SHORTBREAK);
+          short += value;
+          if (short >= 1 && short <= 120) {
+            prefs.setInt(SHORTBREAK, short);
+            setState(() {
+              txtShort.text = short.toString();
+            });
+          }
+        }
+        break;
+      case LONGBREAK:
+        {
+          int long = prefs.getInt(LONGBREAK);
+          long += value;
+          if (long >= 1 && long <= 180) {
+            prefs.setInt(LONGBREAK, long);
+            setState(() {
+              txtLong.text = long.toString();
+            });
+          }
+        }
+        break;
+    }
   }
 }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+typedef CallbackSetting = void Function(String, int);
+
 class ProductivityButton extends StatelessWidget {
   final Color color;
   final String text;
@@ -30,17 +32,20 @@ class SettingButton extends StatelessWidget {
   final Color color;
   final String text;
   final int value;
+  final String setting;
+  final CallbackSetting callback;
 
-  SettingButton(this.color, this.text, this.value);
+  SettingButton(this.color, this.text, this.value, this.setting, this.callback);
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-        child: Text(
-          this.text,
-          style: TextStyle(color: Colors.white),
-        ),
-        color: this.color,
-        onPressed: () => null);
+      child: Text(
+        this.text,
+        style: TextStyle(color: Colors.white),
+      ),
+      color: this.color,
+      onPressed: () => this.callback(this.setting, this.value),
+    );
   }
 }
